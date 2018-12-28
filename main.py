d1f08dbc217201197f490f182af1a5fd71ea58f7
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-12-27 18:30:31 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import base64
 
 from flask import Flask, request
-from model import Message 
+import html
+from model import Message
 
 app = Flask(__name__)
 
@@ -31,12 +32,11 @@ def home():
 <div class="message">
 {}
 </div>
-""".format(m.content)
+""".format(html.escape(m.content))
 
-    return body 
+    return body
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
